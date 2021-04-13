a1dabe459743975c6a8e45f5f20fc678199d5ece
Yusuf Onder <yusuf.onder@appcent.mobi>
2021-04-13 23:40:22 +0300

    Fix quotes error state wrong casting

--- app/src/main/java/com/yonder/breakingbadcompose/ui/quotes/Quotes.kt
+++ app/src/main/java/com/yonder/breakingbadcompose/ui/quotes/Quotes.kt
@@ -1,23 +1,13 @@
 package com.yonder.breakingbadcompose.ui.quotes
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.Divider
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.hilt.navigation.compose.hiltNavGraphViewModel
 import androidx.navigation.NavController
-import com.yonder.breakingbadcompose.ui.characters.CharactersUiState
 import com.yonder.breakingbadcompose.ui.common.ErrorView
 import com.yonder.breakingbadcompose.ui.common.LoadingView
-import com.yonder.breakingbadcompose.ui.theme.padding_normal
 
 @Composable
 fun Quotes(navController: NavController) {
@@ -41,16 +31,11 @@ fun Quotes(navController: NavController) {
         is QuotesUiState.Error -> {
             ErrorView(
                 errorMessage =
-                (quotesUiState as CharactersUiState.Error)
+                (quotesUiState as QuotesUiState.Error)
                     .exception.localizedMessage.orEmpty()
             )
         }
 
     }
 
-
-/*
-    Button(onClick = { navController.navigate("characters") }) {
-        Text(text = "Navigate next")
-    }*/
 }
